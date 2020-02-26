b7fba4899788609516775f27716044c85124f5a4
Eduardo Gallego garcia <egallego@c1r9s3.42madrid.com>
2020-02-26 12:57:05 +0100

    First setup

---
+++ get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: egallego <egallego@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/24 18:50:47 by egallego          #+#    #+#             */
+/*   Updated: 2020/02/24 19:06:30 by egallego         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+/*
+** What libraries do I need?
+*/
+
+
+
+/* 
+** Prototype name
+*/
+
+int get_next_line(int fd, char **line);
+
+#endif
